52758f681911973232c54c3d94ca3749671ca114
elguesabal <joseelguesabal@gmail.com>
2024-11-07 12:24:37 -0300

    corrigindo bugs relacionado a url e parametros (acredito q nao tenha nenhum bug no momento) adicionando preco a mensagem do whatasapp (inclui somatorio de precos tbm)

--- src/componentes/header.jsx
+++ src/componentes/header.jsx
@@ -1,18 +1,42 @@
+import { produtos } from "../produtos.js";
+
 function carrinho() {
 	let carrinho = JSON.parse(decodeURIComponent(new URL(window.location.href).searchParams.get("carrinho")));
 	let url = "https://wa.me/5521984025976?text=Olá, gostaria de mais informações desses produtos:";
+	let subtotal = 0;
 
 	if (!carrinho) {
 		alert("Carrinho vazio!");
 		return ;
 	}
 
-	carrinho.forEach((produto) => {
-		url += encodeURIComponent("\n\n") + produto + ":" + encodeURIComponent("\n");
+	carrinho.forEach((produto, i) => {
+		url += encodeURIComponent("\n\n") + produto + ": R$";
+		for (let i = 0; i < produtos.length; i++) {
+			if (produtos[i].titulo === produto) {
+				url += produtos[i].preco.toFixed(2).replace('.', ',') + encodeURIComponent("\n");
+				subtotal += produtos[i].preco;
+				break;
+			}
+		}
 		url += window.location.origin + "?produto=" + encodeURIComponent(encodeURIComponent(produto));
 	});
+	url += encodeURIComponent("\n\n") + "Clique aqui e volte a adicionar produtos ao seu carrinho:" + encodeURIComponent("\n") + window.location.href;
+	url += encodeURIComponent("\n\n") + "Subtotal: R$" + subtotal.toFixed(2).replace('.', ',');
 
 	window.open(url, "_blank");
+}
+
+function pesquisa(event) {
+	event.preventDefault();
+	if (!event.target.elements.pesquisa.value) return ;
+
+	let url = new URLSearchParams(new URL(window.location.href));
+	const carrinho = new URLSearchParams(new URL(window.location.href).search).get("carrinho");
+
+	url.set("pesquisa", event.target.elements.pesquisa.value);
+	(carrinho) ? url.set("carrinho", carrinho) : "";
+	window.location.href = window.location.origin + "/pesquisa?" + url.toString();
 }
 
 export default function Header() {
@@ -41,7 +65,7 @@ export default function Header() {
 							<a className="nav-link active" aria-current="page" href="#">Roedores</a>
 						</li>
 					</ul>
-					<form className="d-flex" action="/pesquisa" method="get">
+					<form className="d-flex" onSubmit={pesquisa}> {/* action="/pesquisa" method="get" */}
 						<i className="btn btn-outline-success bi bi-cart-check me-3" onClick={carrinho}></i>
 						<input className="form-control me-2" placeholder="Pesquisar" name="pesquisa"/>
 						<button className="btn btn-outline-success" type="submit">Pesquisar</button>
